80d9dc59bc794efef92cc9e5fd3087a71f334261
Toni Regidor <aregidorballesteros@gmail.com>
2026-05-18 01:13:52 +0200

    Reorder photos list and update phrases for consistency

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
    PHOTOS LIST  (alphabetical order)
    ========================================= */
 const photos = [
-  '0-a.jpeg', '0.jpeg',   '10.jpeg',  '14.jpeg',  '15.jpeg',  '16.jpeg',
-  '18.jpeg',  '19.jpeg',  '2.jpeg',   '20.jpeg',  '22.jpeg',  '23.jpeg',
-  '24.jpeg',  '25.jpeg',  '26.jpeg',  '27.jpeg',  '28.jpeg',  '3.jpeg',
-  '30.jpeg',  '31.jpeg',  '32.jpeg',  '33.jpeg',  '34.jpeg',  '35.jpeg',
-  '36.jpeg',  '37.jpeg',  '38.jpeg',  '39.jpeg',  '4.jpeg',   '40.jpeg',
-  '41.jpeg',  '42.jpeg',  '43.jpeg',  '44.jpeg',  '45.jpeg',  '46.jpeg',
-  '47.jpeg',  '48.jpeg',  '5.jpeg',   '51.jpeg',  '52.jpeg',  '53.jpeg',
-  '54.jpeg',  '55.jpeg',  '6.jpeg',   '7.jpeg',   '8-a.jpeg', '8.jpeg',
-  '9-a.jpeg', '9-b.jpeg', '9.jpeg'
+  '0.jpeg', '0-a.jpeg',   '2.jpeg',  '3.jpeg',  '4.jpeg',  '5.jpeg',
+  '6.jpeg',  '7.jpeg',  '8.jpeg',   '8-a.jpeg',  '9.jpeg',  '9-a.jpeg',
+  '9-b.jpeg',  '16.jpeg',  '10.jpeg',  '15.jpeg',  '14.jpeg',  '18.jpeg',
+  '19.jpeg',  '20.jpeg',  '22.jpeg',  '23.jpeg',  '24.jpeg',  '25.jpeg',
+  '26.jpeg',  '27.jpeg',  '28.jpeg',  '30.jpeg',   '31.jpeg',
+  '32.jpeg',  '33.jpeg',  '34.jpeg',  '35.jpeg',  '36.jpeg',  '37.jpeg',
+  '38.jpeg',  '39.jpeg',  '40.jpeg',   '41.jpeg',  '42.jpeg',  '43.jpeg',
+  '44.jpeg',  '45.jpeg',  '46.jpeg',   '47.jpeg',  '48.jpeg',
+  '51.jpeg',  '52.jpeg',   '53.jpeg',  '54.jpeg',  '55.jpeg'
 ];
 
 /* =========================================
@@ -20,8 +20,8 @@ const photos = [
 const phrases = {
    5: "It's already been a year since we first saw each other",
   11: "I fell in love with you because of the way you treat me, with so much love and tenderness",
-  16: "I hit the jackpot when I met you",
-  22: "I didn't hesitate for a minute to marry you",
+  16: "I didn't hesitate for a minute to marry you",
+  22: "I hit the jackpot when I met you",
   27: "We have shared so many great moments together",
   33: "We have visited so many beautiful places together",
   38: "Every minute with you is worth an eternity without you",
